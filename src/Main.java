c04c2475dcdd553d4ff6a3c82ca16a5899121c5d
songjungsung <wjdtjd7642@gmail.com>
2022-12-26 11:09:12 +0900

    넘버 2

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		System.out.println("새로운 내용을 추가");
 		System.out.println("또 다른 내용을 추가");
 		
-		System.out.println("깃허브에 반영할 내용");
+		System.out.println("기트허브에 반영할 내용");
 	}
 
 }
